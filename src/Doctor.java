4e3728ed7bc5dc9cdd9e62b821af53d435739f16
gugarciari-svg <gugarciari@ucvvirtual.edu.pe>
2026-04-30 18:11:14 -0500

    Update Doctor.java

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,15 +1,27 @@
-public class Doctor {
+import java.util.ArrayList;
 
+public class Doctor extends Persona {
 
+    private String especialidad;
+    private String numeroDeLicencia;
+    private ArrayList<Paciente> pacientes;
 
+    public Doctor(String nombre, int edad, String genero, String identificacion,
+                  String especialidad, String numeroDeLicencia) {
+        super(nombre, edad, genero, identificacion);
+        this.especialidad = especialidad;
+        this.numeroDeLicencia = numeroDeLicencia;
+        pacientes = new ArrayList<>();
+    }
 
+    public void agregarPaciente(Paciente paciente) {
+        pacientes.add(paciente);
+    }
 
-
-
-
-
-
-
-
-
+    public void mostrarPacientes() {
+        System.out.println("Pacientes asignados:");
+        for (Paciente p : pacientes) {
+            System.out.println(p.nombre);
+        }
+    }
 }
